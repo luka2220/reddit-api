0ea666a9a4b1fccefbd37cf228175380272f0858
Luka Piplica <piplicaluka64@gmail.com>
2025-02-10 18:46:48 -0500

    Filter locators with no titles and ranks

--- src/main/java/com/piplica/reddit_api/rest/MainController.java
+++ src/main/java/com/piplica/reddit_api/rest/MainController.java
@@ -3,6 +3,7 @@ package com.piplica.reddit_api.rest;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     private final BrowserContext browserCtx;
     private final String redditURL = "https://old.reddit.com/";
+    private final Page page;
 
     @Autowired
     MainController(Browser browser) {
         this.browserCtx = browser.newContext();
+        this.page = browserCtx.newPage();
     }
 
     @GetMapping("/popular")
     String baseURL() {
-        var page = browserCtx.newPage();
         page.navigate(redditURL);
         var listings = page.locator("[data-context='listing']").all();
         StringBuilder response = new StringBuilder();
@@ -30,17 +32,29 @@ public class MainController {
                 continue;
             }
 
-            var title = listing
-                    .locator(":scope [data-event-action='title']")
-                    .textContent();
-            var rank = listing.locator(":scope .rank").textContent();
+            var titleLocator = listing
+                    .locator(":scope [data-event-action='title']");
+            var rankLocator = listing.locator(":scope .rank");
+            var id = listing.getAttribute("id");
 
-            response.append(rank)
+            if (titleLocator.count() == 0 || rankLocator.count() == 0) continue;
+
+
+            response.append(rankLocator.textContent())
                     .append(" - ")
-                    .append(title)
+                    .append(id)
+                    .append(" - ")
+                    .append(titleLocator.textContent())
                     .append("<br>");
         }
 
         return response.toString();
+    }
+
+    // TODO: Properly parse out id from string thing_t3_i11iu23 the last word is the id...
+    private String cleanId(String id) {
+        var arr = id.split("_");
+        System.out.println(arr);
+        return "";
     }
 }
